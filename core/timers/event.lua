4c9e1ff4bc068da5beed7baf8b1c8a98b9992fea
logic-automation <144732884+logic-automation@users.noreply.github.com>
2025-05-17 22:12:28 -0700

    Update event.lua

--- core/timers/event.lua
+++ core/timers/event.lua
@@ -1,52 +1,63 @@
-local config  = require('lib.giko.config')
-local common  = require('lib.giko.common')
-local sound   = require('lib.giko.sound')
-local event   = {}
+local config     = require('lib.giko.config')
+local common     = require('lib.giko.common')
+local sound      = require('lib.giko.sound')
+local event      = {}
 
 event.get_timers = function()
-    
     local timers = {}
 
     if config.timers.events then
+        for k, event_item in ipairs(config.timers.events) do
+            if event_item.enabled then
+                local time = os.time()
+                local dst  = os.date('*t').isdst
+                local y, m, d, h, m, s, a, countdown
 
-        for k, event in ipairs(config.timers.events) do
+                while countdown == nil do
+                    for l, timer in ipairs(event_item.times) do
+                        y, m, d, a, A = string.match(os.date('%Y-%m-%d %a %A', time),
+                            '(%d%d%d%d)%-(%d%d)%-(%d%d)%s(%w+)%s(%w+)')
 
-            local time = os.time()
-            local dst  = os.date('*t').isdst
-            local y, m, d, h, m, s, a, countdown
+                        event_at = os.time({
+                                year = y,
+                                month = m,
+                                day = d,
+                                hour = string.gsub(timer.gmt, '%:.*', ''),
+                                min =
+                                    string.gsub(timer.gmt, '.*%:', ''),
+                                sec = 0
+                            }) +
+                            common.offset_to_seconds(os.date('%z', os.time()))
 
-            while countdown == nil do
-
-                for l, timer in ipairs(event.times) do
-                
-                    y, m, d, a, A = string.match(os.date('%Y-%m-%d %a %A', time), '(%d%d%d%d)%-(%d%d)%-(%d%d)%s(%w+)%s(%w+)') 
-
-                    event_at = os.time({year = y, month = m, day = d, hour = string.gsub(timer.gmt, '%:.*', ''), min = string.gsub(timer.gmt, '.*%:', ''), sec = 0}) + common.offset_to_seconds(os.date('%z', os.time()))
-                    
-                    if string.lower(A) == string.lower(timer.day) and event_at > os.time() then
-                        countdown = os.difftime(event_at, os.time())
-                        break
+                        if string.lower(A) == string.lower(timer.day) and event_at > os.time() then
+                            countdown = os.difftime(event_at, os.time())
+                            break
+                        end
                     end
 
+                    time = time + 86400
                 end
 
-                time = time + 86400
-            end
+                if countdown - 1 == 60 and not config.ui.muted then
+                    sound.call(config.ui.sound.call or 1, config.ui.sound.volume or 5, config.ui.sound.lib or 'giko.call')
+                end
 
-            if countdown - 1 == 60 and not config.ui.muted then
-                sound.call(config.ui.sound.call or 1, config.ui.sound.volume or 5, config.ui.sound.lib or 'giko.call')
+                if ui.hover or countdown < (event_item.visible_at and common.to_seconds(event_item.visible_at) or 3600) then
+                    table.insert(timers,
+                        {
+                            time = os.date('%m-%d %H:%M:%S', event_at),
+                            countdown = math.max(countdown, 0),
+                            lbl = string
+                                .format('%s - %s',
+                                    config.ui.mode ~= "time" and common.to_time(math.max(countdown, 0)) or
+                                    os.date('%m-%d %H:%M:%S', event_at), event_item.lbl)
+                        })
+                end
             end
-
-            if ui.hover or countdown < (event.visible_at and common.to_seconds(event.visible_at) or 3600) then
-                table.insert(timers, {time = os.date('%m-%d %H:%M:%S', event_at), countdown = math.max(countdown, 0), lbl = string.format('%s - %s', config.ui.mode ~= "time" and common.to_time(math.max(countdown, 0)) or os.date('%m-%d %H:%M:%S', event_at), event.lbl)})
-            end
-
         end
-
     end
 
     return timers
-        
 end
 
 return event
